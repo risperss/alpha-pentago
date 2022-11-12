eaa559ad08fdd87132f5c60519a88a648d6041f3
risperss <gaetanorispoli@outlook.com>
2022-11-12 17:49:51 -0500

    refactor + readme

--- pentago.py
+++ pentago.py
@@ -1,37 +1,33 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class Node:
-    white: bool = None
-
-    def __str__(self):
-        if self.white is None:
-            return "x"
-        elif self.white:
-            return "w"
-        else:
-            return "b"
-
+NODE_FORMAT = {
+    None: ".",
+    True: "r",
+    False: "b",
+}
 
 class Square:
-    nodes: list[list[Node]]
+    nodes: list[list[bool]]
 
     def __init__(self):
         self.nodes = [
-            [Node(), Node(), Node()],
-            [Node(), Node(), Node()],
-            [Node(), Node(), Node()],
+            [None, None, None],
+            [None, None, None],
+            [None, None, None],
         ]
 
-    def rotate_clockwise(self):
+    def rotate(self, clockwise: bool):
+        if clockwise:
+            self._rotate_clockwise()
+        else:
+            self._rotate_counterclockwise()
+
+    def _rotate_clockwise(self):
         self.nodes = [
             [self.nodes[2][0], self.nodes[1][0], self.nodes[0][0]],
             [self.nodes[2][1], self.nodes[1][1], self.nodes[0][1]],
             [self.nodes[2][2], self.nodes[1][2], self.nodes[0][2]],
         ]
 
-    def rotate_counterclockwise(self):
+    def _rotate_counterclockwise(self):
         self.nodes = [
             [self.nodes[0][2], self.nodes[0][1], self.nodes[0][0]],
             [self.nodes[1][2], self.nodes[1][1], self.nodes[1][0]],
@@ -42,7 +38,8 @@ class Square:
         s = ""
         for row in self.nodes:
             for node in row:
-                s += str(node)
+                s += NODE_FORMAT[node] + " "
+            s += "\n"
         return s
 
 
@@ -60,4 +57,5 @@ class Board:
         for row in self.squares:
             for square in row:
                 s += str(square)
+            s += "\n"
         return s
--- tests/test_pentago.py
+++
@@ -1,6 +0,0 @@
-from pentago.pentago import Node, Square, Board
-
-
-def test_encoding():
-    board = Board()
-    assert str(board) == "x"*36
