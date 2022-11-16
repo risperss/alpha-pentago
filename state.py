67a007015be4f69afac3dff777581796758c914d
risperss <gaetanorispoli@outlook.com>
2022-11-16 14:00:41 -0500

    saving ab pruning state

--- state.py
+++ state.py
@@ -1,12 +1,13 @@
 from pentago import Board
 import random
 
-
 class State():
     board: Board
+    checks: int
 
     def __init__(self):
         self.board = Board()
+        self.checks = 0
 
     def serialize(self):
         ...
@@ -14,8 +15,9 @@ class State():
     def edges(self):
         return self.board.legal_moves()
 
-    def value(self):
-        return self.minimax(36, self.board.current_player)
+    def value(self, d: int):
+        self.checks = 0
+        return self.minimax(d, self.board.current_player)
 
     def heuristic_value(self):
         # First simple solution: Computing percentage of 16 central squares occupied
@@ -35,6 +37,8 @@ class State():
     def minimax(self, depth, red, alpha=-2, beta=2):
         outcome = self.board.outcome()
         if depth == 0 or outcome is not None:
+            self.checks += 1
+            print(self.checks)
             if outcome == Board.Outcome.RED_WIN:
                 return 1
             elif outcome == Board.Outcome.BLACK_WIN:
@@ -46,23 +50,23 @@ class State():
 
         if red:
             max_eval = -2
-            for move in self.edges():
+            for move in self.board.legal_moves():
                 self.board.push(move)
-                eval = self.minimax(depth-1, alpha, beta, not red)
+                eval_ = self.minimax(depth-1, alpha, beta, not red)
                 self.board.pop()
-                max_eval = max(max_eval, eval)
-                alpha = max(alpha, eval)
+                max_eval = max(max_eval, eval_)
+                alpha = max(alpha, eval_)
                 if beta <= alpha:
                     break
             return max_eval
         else:
             min_eval = 2
-            for move in self.edges():
+            for move in self.board.legal_moves():
                 self.board.push(move)
-                eval = self.minimax(depth-1, alpha, beta, not red)
+                eval_ = self.minimax(depth-1, alpha, beta, not red)
                 self.board.pop()
-                min_eval = min(min_eval, eval)
-                beta = min(beta, eval)
+                min_eval = min(min_eval, eval_)
+                beta = min(beta, eval_)
                 if beta <= alpha:
                     break
             return min_eval
@@ -70,4 +74,6 @@ class State():
 
 if __name__ == "__main__":
     s = State()
-    print(s.value())
+    for i in range(9, 10):
+        s.value(i)
+        print(i, s.checks)
