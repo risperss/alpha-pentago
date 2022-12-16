af985a8c81932c966496c30e866e0548dee97647
risperss <gaetanorispoli@outlook.com>
2022-12-16 15:29:10 -0500

    Add untracked files

---
+++ src/neural/cmaes.cc
@@ -0,0 +1,73 @@
+#include "cmaes.h"
+
+#include <cmath>
+#include <iostream>
+#include <random>
+#include <vector>
+
+#include "neural/heuristic.h"
+#include "neural/minimax.h"
+#include "pentago/position.h"
+
+namespace pentago {
+
+PositionHistory randomSelfPlay() {
+  PentagoBoard board = PentagoBoard();
+  Position starting = Position(board);
+  PositionHistory history = PositionHistory(starting);
+
+  while (history.ComputeGameResult() == GameResult::UNDECIDED) {
+    MoveList legalMoves = history.Last().GetBoard().GenerateLegalMoves();
+
+    // randomize
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    shuffle(legalMoves.begin(), legalMoves.end(),
+            std::default_random_engine(seed));
+
+    history.Append(legalMoves.front());
+  }
+
+  return history;
+}
+
+float regret(HeuristicEvaluator* evaluator, Position position) {
+  PositionLookup lookup;
+  int nodesVisited = 0;
+
+  float heuristicValue = evaluator->value(position);
+  ReturnValue returnValue =
+      minimax(position, INT_MAX, &lookup, &nodesVisited, evaluator);
+  float regret = std::abs(returnValue.value - heuristicValue);
+
+  std::cout << "Heuristic: " << heuristicValue << std::endl;
+  std::cout << "Value: " << returnValue.value << std::endl;
+  std::cout << "Regret: " << regret << std::endl;
+  std::cout << "---------------------------" << std::endl;
+
+  return regret;
+}
+
+float fitness(std::array<float, kNumWeights> weights) {
+  HeuristicEvaluator* evaluator = new HeuristicEvaluator(weights);
+  float sumRegret = 0.0;
+  int sampleCount = 5;
+
+  int i = 1;
+  while (i <= sampleCount) {
+    PositionHistory history = randomSelfPlay();
+
+    if (history.Last().GetPlyCount() >= 33) {
+      sumRegret +=
+          regret(evaluator, history.GetPositionAt(history.GetLength() - 4));
+      i++;
+    }
+  }
+  delete evaluator;
+
+  float averageRegret = sumRegret / sampleCount;
+
+  std::cout << "Average Regret: " << averageRegret << std::endl;
+
+  return averageRegret;
+}
+}  // namespace pentago
---
+++ src/neural/cmaes.h
@@ -0,0 +1,12 @@
+#include <array>
+
+#include "neural/heuristic.h"
+#include "pentago/position.h"
+
+namespace pentago {
+PositionHistory randomSelfPlay();
+float regret(HeuristicEvaluator* evaluator, PositionHistory history);
+float fitness(std::array<float, kNumWeights> weights);
+
+std::array<float, kNumWeights> cmaes();
+}  // namespace pentago
